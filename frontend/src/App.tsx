d663ca5e892bbefe48d99ac56f67fdccc25a2597
Candace <candacecsy@gmail.com>
2022-12-06 17:13:21 -0500

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Home } from './pages/Home';
-import { TestTimeline } from './pages/Timeline';
 
 function App() {
   return (
