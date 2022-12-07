b03307263d63d5e4b434cdb261b74a3f7032f5fc
Duncan Tang <pofungt@hotmail.com>
2022-12-07 15:56:09 -0500

    Auth.tsx header

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,10 +1,31 @@
+import { createStyles } from '@mantine/core';
 import React from 'react';
+import { Logo } from '../components/Logo';
 import { loginThunk } from '../redux/auth/thunk';
 import { useAppDispatch, useAppSelector } from '../store';
+
+const useStyles = createStyles(theme => ({
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    header: {
+        width: '100%',
+        height: 100,
+        backgroundColor: theme.colors.lightViolet,
+        padding: 15,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+}))
 
 export function Auth() {
     const user = useAppSelector(state => state.auth.username);
     const dispatch = useAppDispatch();
+    const {classes} = useStyles();
 
     const login = (event: any) => {
         event.preventDefault();
@@ -12,8 +33,11 @@ export function Auth() {
     }
 
     return (
-        <div>
+        <div className={classes.wrapper}>
             {user.length > 0 && <div>Hi {user}!</div>}
+            <div className={classes.header}>
+                <Logo size={50}/>
+            </div>
             <form onSubmit={login}>
                 <label htmlFor="username">Username</label>
                 <input name="username" id="username"></input>
