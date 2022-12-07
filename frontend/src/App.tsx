350d7af5d08103adfcfd83e9bb155550bf737036
Duncan Tang <pofungt@hotmail.com>
2022-12-07 13:41:44 -0500

    app.tsx login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.scss';
 import { Auth } from './pages/Auth';
+import { Home } from './pages/Home';
+import { useAppSelector } from './store';
 
 function App() {
+  const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn);
   return (
     <div className="App">
       <div className='page-container'>
-        <Auth/>
+        {isLoggedIn ? <Auth/> : <Home/>}
       </div>
     </div>
   );
