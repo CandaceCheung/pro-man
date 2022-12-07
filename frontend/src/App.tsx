2e6e471e32ea9ed6878dddfefefddf5472fe5025
Yip Chak Ming, Frankie <cmyip6@gmail.com>
2022-12-07 14:01:58 -0500

    show Navbar after Login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.scss';
+import ProjectNavbar from './components/ProjectNavbar';
 import { Auth } from './pages/Auth';
+import { IRootState } from './store';
 
 function App() {
+
+  const isLoggedIn = useSelector((state: IRootState) => state.auth.isLoggedIn)
+
   return (
     <div className="App">
       <div className='page-container'>
-        <Auth/>
+        {isLoggedIn ? <ProjectNavbar/>:<Auth/>}
       </div>
     </div>
   );
