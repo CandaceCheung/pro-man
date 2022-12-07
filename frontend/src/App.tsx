e935f80062e2f18e7095cc13ddf67fd51b9eb499
Candace <candacecsy@gmail.com>
2022-12-07 13:36:42 -0500

    add left navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import { LeftNavbar } from './components/LeftNavbar';
 import ProjectNavbar from './components/ProjectNavbar';
 
 function App() {
   return (
     <div className="App">
+      <LeftNavbar />
       <ProjectNavbar/>
     </div>
   );
