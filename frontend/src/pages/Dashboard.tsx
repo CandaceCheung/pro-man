62ed7542925dd9eeda46658598a3ea99dd50e57f
Yip Chak Ming, Frankie <cmyip6@gmail.com>
2022-12-08 12:52:29 -0500

    fixed seed file and added back-end controller, service and routes

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavbarLayout } from "../components/NavbarLayout";
+import { getTable } from "../redux/table/thunk";
+import { useAppDispatch, useAppSelector } from "../store";
 
 export function Dashboard() {
+    const dispatch = useAppDispatch();
+    const userId = useAppSelector((state) => state.auth.userId)
+
+    useEffect(()=>{
+        dispatch(getTable(userId as number))
+    },[dispatch, userId])
+
     return (
         <div>
             <NavbarLayout />
