3343c2c5f533c6a0ba69464c42087288ee12d1fa
Candace <candacecsy@gmail.com>
2022-12-08 09:41:52 -0500

    added Route of the left navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import { Auth } from "./pages/Auth";
 import { useAppSelector } from "./store";
+import { Dashboard } from "./pages/Dashboard";
+import { Profile } from "./pages/Profile";
 
 function App() {
     const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
@@ -13,6 +15,11 @@ function App() {
             {isLoggedIn ? (
                 <Routes>
                     <Route path="/home" element={<Home />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/notification" />
+                    <Route path="/my-work" />
+                    <Route path="/favorite" />
+                    <Route path="/profile" element={<Profile />} />
                 </Routes>
             ) : (
                 <Auth />
