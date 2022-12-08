1cb2583b844eddffec6343f297d11367717570e3
Candace <candacecsy@gmail.com>
2022-12-08 16:41:39 -0500

    kanban element update

---
+++ frontend/src/components/KanbanComponent/StatusColumn.tsx
@@ -0,0 +1,8 @@
+export function StatusColumn () {
+    return (
+        <>
+        <div className="statusColumn">Status / {} </div>
+        
+        </>
+    )
+}
--- frontend/src/pages/Kanban.tsx
+++ frontend/src/pages/Kanban.tsx
@@ -1,10 +1,11 @@
+import { StatusColumn } from "../components/KanbanComponent/StatusColumn";
 import { NavbarLayout } from "../components/NavbarLayout";
 
-
-export function Kanban(){
-     return(
+export function Kanban() {
+    return (
         <div>
             <NavbarLayout />
+            <StatusColumn />
         </div>
-     );
-};
+    );
+}
