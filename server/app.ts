07448a561469efdd601870e03305113695dfc490
Candace <candacecsy@gmail.com>
2022-12-09 15:19:25 -0500

    added kanban API

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import Knex from 'knex';
 import { AuthService } from './services/authServices';
 import { AuthController } from './controllers/authControllers';
@@ -7,6 +7,7 @@ import { authRoutes } from './routes/authRoutes';
 import { TableService } from './services/tableService';
 import { TableController } from './controllers/tableControllers';
 import { tableRoutes } from './routes/tableRoutes';
+import { KanbanService } from './services/KanbanService';
 import cors from 'cors';
 
 dotenv.config();
@@ -15,7 +16,7 @@ const knexConfig = require('./knexfile');
 const configMode = process.env.NODE_ENV || 'development';
 export const knex = Knex(knexConfig[configMode]);
 
-const app = express()
+const app = express();
 
 export const authService = new AuthService(knex);
 export const authController = new AuthController(authService);
@@ -23,6 +24,7 @@ export const authController = new AuthController(authService);
 export const tableService = new TableService(knex);
 export const tableController = new TableController(tableService);
 
+export const kanbanService = new KanbanService(knex);
 
 app.use(express.json(), cors());
 
