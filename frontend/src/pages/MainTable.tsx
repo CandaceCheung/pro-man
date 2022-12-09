5245065b9a7e8fb062a2f6c48459445f6464bc71
Duncan Tang <pofungt@hotmail.com>
2022-12-09 17:40:01 -0500

    main table

--- frontend/src/pages/MainTable.tsx
+++ frontend/src/pages/MainTable.tsx
@@ -2,8 +2,6 @@ import {  useAppSelector } from "../store";
 
 export function MainTable() {
     
-    const projectDetail = useAppSelector((state) => state.table)
-
     return (
         <div className="tab-content">
             Example
