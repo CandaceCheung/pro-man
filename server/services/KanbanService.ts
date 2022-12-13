32b9974beff67db43b6cb919113dcaf2601d8ba0
Candace <candacecsy@gmail.com>
2022-12-13 14:42:46 -0500

    update

--- server/services/KanbanService.ts
+++ server/services/KanbanService.ts
@@ -3,24 +3,35 @@ import { Knex } from 'knex';
 export class KanbanService {
 	constructor(private knex: Knex) {}
 
-	async getMemberInfo(item_id: number) {
-		const itemCreator = await this.knex
-			.select('users.id as user_id')
-			.from('items')
-			.join('users', '', '=', '')
-			.where('user');
-
-		return itemCreator;
-	}
-
 	async getKanbanInfo(project_id: number) {
 		const kanbanDetail = await this.knex
-			.select('projects.id as project_id')
+			.select(
+				'projects.id as project_id',
+				'items.id as item_id',
+				'items.name as item_name',
+				'type_dates.datetime as item_dates_datetime',
+				'states.id as state_id',
+				'states.name as item_status_name',
+				'states.color as itemStatusColor',
+				'kanban_order.item_id_order as kanbanColumnOrder'
+			)
 			.from('states')
-			.join('type_status', 'type_status.state_id', '=', 'states.id')
 			.join('projects', 'states.project_id', '=', 'projects.id')
-			.where('projects.id', '=', project_id);
+			.join('type_status', 'type_status.state_id', '=', 'states.id')
+			.join('items','type_status.item_id', '=', 'items.id')
+			.join('type_dates', 'type_dates.item_id', '=', 'items.id')
+			.join('kanban_order', 'kanban_order.state_id', '=', 'states.id')
+			.where('projects.id', project_id);
+			
 
 		return kanbanDetail;
+	}
+
+	async changeKanbanOrder(kanban_order: number) {
+		const column_order = await this.knex('kanban_order').where(
+			'kanban_order.id'
+		);
+
+		return column_order;
 	}
 }
