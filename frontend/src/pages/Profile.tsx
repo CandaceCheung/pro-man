266c47026653c9711ebb87374d1a6f547ea925d4
Candace <candacecsy@gmail.com>
2022-12-16 22:37:49 -0500

    draft update

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,10 +1,25 @@
-import React from "react"
+import { Button, Group, Input, Text } from "@mantine/core";
+import { IconAt, IconKey, IconUserCircle } from "@tabler/icons";
+import React from "react";
 
 export function Profile() {
-
-    return(
+    return (
         <div>
-            This is a profile page.
+            <Group position="center">
+                <IconUserCircle size={100} />
+            </Group>
+            <Text>User Name</Text>
+            <Group>
+                <Text>Email:</Text>
+                <Input icon={<IconAt size={16} />}></Input>
+            </Group>
+            <Group>
+                <Text>Password</Text>
+                <Input icon={<IconKey size={16} />}></Input>
+            </Group>
+            <Button color="cyan" mt={5}>
+                Update
+            </Button>
         </div>
-    )
-} 
+    );
+}
