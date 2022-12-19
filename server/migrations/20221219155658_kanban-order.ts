9cabc3896c27efc55233cedbde14f2527e479ec5
Candace <candacecsy@gmail.com>
2022-12-19 13:01:31 -0500

    kanban order migration

---
+++ server/migrations/20221219155658_kanban-order.ts
@@ -0,0 +1,20 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.table("states", (table) => {
+        table.integer('status_order');
+    })
+
+    await knex.schema.dropTable("kanban_order");
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.table("states",(table) => {
+        table.integer('status_order_id');
+    })
+
+    await knex.schema.createTable("kanban_order", () => {});
+}
+
