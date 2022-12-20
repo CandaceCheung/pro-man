a8f3e00898d19e05ed4f2e14d0d8f159fdade5f1
Candace <candacecsy@gmail.com>
2022-12-20 14:50:04 -0500

    comment the thunk

--- frontend/src/redux/profile/thunk.ts
+++ frontend/src/redux/profile/thunk.ts
@@ -1,7 +1,22 @@
 import { Dispatch } from "@reduxjs/toolkit";
 
-export function getprofile () {
+export function getProfile (userId: number) {
     return async (dispatch: Dispatch) => {
-        
+        // const token = localStorage.getItem('token');
+
+        // const res = await fetch(
+        //     `${process.env.REACT_APP_API_SERVER}/profile/${userId}`,
+        //     {
+        //         headers: {
+        //             'Authorization': `Bearer ${token}`,
+        //         },
+        //     }
+        // );
+
+        // const result: { ; success: boolean } =
+        // await res.json();
+
+
+
     }
 }
