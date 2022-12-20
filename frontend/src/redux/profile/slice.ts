c60903fed13d773e120450ac940a022ecfb5ba0a
Candace <candacecsy@gmail.com>
2022-12-20 12:20:17 -0500

    profile slice

--- frontend/src/redux/profile/slice.ts
+++ frontend/src/redux/profile/slice.ts
@@ -1,7 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
 
 export interface ProfileState {
     userId: number,
     username: string,
     firstName: string,
     lastName: string,
-}
+};
+
+const initialState: ProfileState = {
+    userId: 0,
+    username: '',
+    firstName: '',
+    lastName: '',
+};
+
+
+
+const profileSlice = createSlice({
+    name: 'profile',
+    initialState,
+    reducers: {
+        
+    }
+});
+
+export const {
+
+} = profileSlice.actions
+
+export default profileSlice.reducer
+
