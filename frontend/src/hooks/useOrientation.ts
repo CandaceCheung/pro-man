570af058702f093f16527040b0fc67fc0eba0d1d
Duncan Tang <pofungt@hotmail.com>
2023-02-01 14:27:22 -0500

    orientation fix on useOrientation

--- frontend/src/hooks/useOrientation.ts
+++ frontend/src/hooks/useOrientation.ts
@@ -3,9 +3,7 @@ import { useEffect } from "react";
 export const useOrientation = (setLandscape: React.Dispatch<React.SetStateAction<boolean>>) => {
     useEffect(() => {
         function handleOrientationChange() {
-            // these are width and height of original screen
-            // so when original innerWidth < original innerHeight, new orientation will be landscape
-            setLandscape(window.innerWidth < window.innerHeight);
+            setLandscape(window.innerWidth > window.innerHeight);
         }
         window.addEventListener('orientationchange', handleOrientationChange);
         return () => {
